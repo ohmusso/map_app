277d3552b0964ec19672e7a979d0402ebb525dc5
ohmusso <itp903nc@gmail.com>
2024-07-03 23:44:49 +0900

    drawer

--- app_map/lib/vecmap_widget.dart
+++ app_map/lib/vecmap_widget.dart
@@ -11,6 +11,8 @@ import 'grid_widget.dart';
 import 'webapi.dart';
 import 'input_latlng_widget.dart';
 
+/// package:latlng 地図の空間座標系
+/// 緯度経度から地図の座標を計算したり、その逆を行うために使用する
 final _epsg = EPSG4326();
 
 Future<Tile> getTileFromPbf(
@@ -43,8 +45,8 @@ class Demo extends StatefulWidget {
 class _DemoState extends State<Demo> {
   final ValueNotifier<InputLatLng> vnInputLatLng =
       ValueNotifier<InputLatLng>(InputLatLng(34.661791, 135.083908));
-  final ValueNotifier<List<Tile_Layer>> vnLayers =
-      ValueNotifier<List<Tile_Layer>>(List.empty());
+  final ValueNotifier<List<VecmapDrawer>> vnDrawers =
+      ValueNotifier<List<VecmapDrawer>>(List.empty());
   CustomPainter? painter = null;
 
   Map<String, List<DrawStyle>> mapDrawStyles = {};
@@ -60,14 +62,86 @@ class _DemoState extends State<Demo> {
     final tileIndex = _epsg.toTileIndexZoom(latLng, zoomLevel);
     final Tile tile = await getTileFromPbf(
         zoomLevel.floor(), tileIndex.x.floor(), tileIndex.y.floor());
-
-    vnLayers.value = tile.layers;
-    mapDrawStyles = await getStyleFromJson('std.json');
     print('read pbf finish');
 
-    painter = MyPainter(mapDrawStyles, vnLayers: vnLayers);
+    print('read style');
+    mapDrawStyles = await getStyleFromJson('std.json');
+
+    print('generate drawer');
+    List<VecmapDrawer> drawers = List.empty(growable: true);
+    Tile_Layer? layer;
+    layer = tile.layers.where((layer) => layer.name == 'waterarea').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer =
+        tile.layers.where((layer) => layer.name == 'wstructurea').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'boundary').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'label').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'elevation').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'river').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'road').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'railway').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'transp').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'landforma').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    layer = tile.layers.where((layer) => layer.name == 'symbol').firstOrNull;
+    drawers.addAll(_genDrawer(layer));
+
+    vnDrawers.value = drawers;
 
     setState(() {});
+  }
+
+  List<VecmapDrawer> _genDrawer(Tile_Layer? layer) {
+    if (layer == null) {
+      return List.empty();
+    }
+
+    List<VecmapDrawer> drawers = List.empty(growable: true);
+    for (var feature in layer.features) {
+      final featureTags = genFeatureTags(layer, feature);
+      final drawStyles = mapDrawStyles[layer.name];
+      final drawStyle = getDrawStyle(drawStyles, feature, featureTags);
+
+      if (drawStyle == null) {
+        // style is not found, do not draw
+        // TODO report not found style
+        continue;
+      }
+
+      final commands = GeometryCommand.newCommands(feature.geometry);
+      switch (feature.type) {
+        case Tile_GeomType.POINT:
+          drawers.add(VecmapPointsDrawer(commands, drawStyle));
+          break;
+        case Tile_GeomType.LINESTRING:
+          drawers.add(VecmapLinestringDrawer(commands, drawStyle));
+          break;
+        case Tile_GeomType.POLYGON:
+          drawers.add(VecmapPolygonDrawer(commands, drawStyle));
+          break;
+        default:
+      }
+    }
+
+    return drawers;
   }
 
   @override
@@ -80,6 +154,8 @@ class _DemoState extends State<Demo> {
     );
 
     _fetchPbf();
+
+    painter = MyPainter(vnDrawers: vnDrawers);
 
     super.initState();
   }
@@ -108,13 +184,163 @@ class _DemoState extends State<Demo> {
   }
 }
 
-class MyPainter extends CustomPainter {
-  final ValueNotifier<List<Tile_Layer>> vnLayers;
-  Map<String, List<DrawStyle>> mapDrawStyles;
+abstract class VecmapDrawer {
+  void vecmapDraw(Canvas canvas);
+}
 
-  MyPainter(this.mapDrawStyles, {required this.vnLayers})
-      : super(repaint: vnLayers) {
-    vnLayers.addListener(() {
+/// https://github.com/mapbox/vector-tile-spec/blob/master/2.1/README.md#4352-example-multi-point
+class VecmapPointsDrawer implements VecmapDrawer {
+  final Paint paint;
+  final List<Offset> offsets;
+
+  factory VecmapPointsDrawer(
+    List<GeometryCommand> commands,
+    DrawStyle drawStyle,
+  ) {
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10.0
+      ..color = Color.fromARGB(255, 0, 140, commands.hashCode);
+
+    final List<Offset> offsets = List.empty(growable: true);
+    for (var command in commands) {
+      if (command.commandType == GeometryCommandType.moveTo) {
+        Offset offset = Offset(0, 0);
+        final itOffsets = command.commandParameters.map((param) {
+          offset = offset.translate(param.x.toDouble(), param.y.toDouble());
+          return offset;
+        });
+
+        offsets.addAll(itOffsets);
+      } else {
+        // TODO error report
+      }
+    }
+
+    return VecmapPointsDrawer._(paint, offsets);
+  }
+
+  VecmapPointsDrawer._(this.paint, this.offsets);
+
+  @override
+  void vecmapDraw(Canvas canvas) {
+    canvas.drawPoints(PointMode.points, offsets, paint);
+  }
+}
+
+/// https://github.com/mapbox/vector-tile-spec/blob/master/2.1/README.md#4353-example-linestring
+class VecmapLinestringDrawer implements VecmapDrawer {
+  final Paint paint;
+  final Path path;
+
+  factory VecmapLinestringDrawer(
+    List<GeometryCommand> commands,
+    DrawStyle drawStyle,
+  ) {
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = drawStyle.lineWidth!.getWidth()
+      ..color = drawStyle.color;
+
+    final Path path = Path();
+    Offset offset = const Offset(0.0, 0.0);
+    for (var command in commands) {
+      switch (command.commandType) {
+        case GeometryCommandType.moveTo:
+          offset = _drawGeoMoveTo(path, command.commandParameters, offset);
+          break;
+        case GeometryCommandType.lineTo:
+          offset = _drawGeoLineTo(path, command.commandParameters, offset);
+          break;
+        case GeometryCommandType.closePath:
+          // nop
+          break;
+        default:
+      }
+    }
+
+    return VecmapLinestringDrawer._(paint, path);
+  }
+
+  VecmapLinestringDrawer._(this.paint, this.path);
+
+  @override
+  void vecmapDraw(Canvas canvas) {
+    canvas.drawPath(path, paint);
+  }
+}
+
+/// https://github.com/mapbox/vector-tile-spec/blob/master/2.1/README.md#4355-example-polygon
+class VecmapPolygonDrawer implements VecmapDrawer {
+  final Paint paint;
+  final Path path;
+
+  factory VecmapPolygonDrawer(
+    List<GeometryCommand> commands,
+    DrawStyle drawStyle,
+  ) {
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 10.0
+      ..color = drawStyle.color;
+
+    final Path path = Path();
+    Offset offset = const Offset(0.0, 0.0);
+    for (var command in commands) {
+      switch (command.commandType) {
+        case GeometryCommandType.moveTo:
+          offset = _drawGeoMoveTo(path, command.commandParameters, offset);
+          break;
+        case GeometryCommandType.lineTo:
+          offset = _drawGeoLineTo(path, command.commandParameters, offset);
+          break;
+        case GeometryCommandType.closePath:
+          path.close();
+          break;
+        default:
+      }
+    }
+
+    return VecmapPolygonDrawer._(paint, path);
+  }
+
+  VecmapPolygonDrawer._(this.paint, this.path);
+
+  @override
+  void vecmapDraw(Canvas canvas) {
+    canvas.drawPath(path, paint);
+  }
+}
+
+/// vecmap drawer helper
+Offset _drawGeoMoveTo(Path path, List<Point<int>> cmdParams, Offset offset) {
+  Offset curOffset = offset;
+
+  for (var param in cmdParams) {
+    curOffset = curOffset.translate(param.x.toDouble(), param.y.toDouble());
+    path.moveTo(curOffset.dx, curOffset.dy);
+  }
+
+  return curOffset;
+}
+
+/// vecmap drawer helper
+Offset _drawGeoLineTo(Path path, List<Point<int>> cmdParams, Offset offset) {
+  Offset curOffset = offset;
+
+  for (var param in cmdParams) {
+    curOffset = curOffset.translate(param.x.toDouble(), param.y.toDouble());
+    path.lineTo(curOffset.dx, curOffset.dy);
+  }
+
+  return curOffset;
+}
+
+class MyPainter extends CustomPainter {
+  final ValueNotifier<List<VecmapDrawer>> vnDrawers;
+
+  MyPainter({required this.vnDrawers}) : super(repaint: vnDrawers) {
+    vnDrawers.addListener(() {
       print('repaint');
     });
   }
@@ -137,169 +363,12 @@ class MyPainter extends CustomPainter {
 
     canvas.scale(scale, scale);
 
-    List<Tile_Layer> layers = vnLayers.value;
-    Tile_Layer? layer;
-    layer = layers.where((layer) => layer.name == 'waterarea').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'wstructurea').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'boundary').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'label').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'elevation').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'river').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'road').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'railway').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'transp').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'landforma').firstOrNull;
-    _drawFeatures(canvas, layer);
-
-    layer = layers.where((layer) => layer.name == 'symbol').firstOrNull;
-    _drawFeatures(canvas, layer);
+    // タイルを描画
+    for (var drawer in vnDrawers.value) {
+      drawer.vecmapDraw(canvas);
+    }
 
     canvas.restore();
-  }
-
-  /// 地物の描画
-  void _drawFeatures(Canvas canvas, Tile_Layer? layer) {
-    if (layer == null) {
-      return;
-    }
-
-    for (var feature in layer.features) {
-      final featureTags = genFeatureTags(layer, feature);
-      final drawStyles = mapDrawStyles[layer.name];
-      final drawStyle = getDrawStyle(drawStyles, feature, featureTags);
-      if (drawStyle == null) {
-        // style is not found, do not draw
-        return;
-      }
-
-      final commands = GeometryCommand.newCommands(feature.geometry);
-      switch (feature.type) {
-        case Tile_GeomType.POINT:
-          _drawPoints(canvas, commands, drawStyle);
-          break;
-        case Tile_GeomType.LINESTRING:
-          _drawStringLine(canvas, commands, drawStyle);
-          break;
-        case Tile_GeomType.POLYGON:
-          _drawPolygon(canvas, commands, drawStyle);
-          break;
-        default:
-      }
-    }
-  }
-
-  void _drawPoints(
-      Canvas canvas, List<GeometryCommand> commands, DrawStyle drawStyle) {
-    final paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 10.0
-      ..color = Color.fromARGB(255, 0, 140, commands.hashCode);
-
-    for (var command in commands) {
-      if (command.commandType == GeometryCommandType.moveTo) {
-        Offset offset = Offset(0, 0);
-        final offsets = command.commandParameters.map((param) {
-          offset = offset.translate(param.x.toDouble(), param.y.toDouble());
-          return offset;
-        }).toList();
-
-        canvas.drawPoints(PointMode.points, offsets, paint);
-      }
-    }
-  }
-
-  void _drawStringLine(
-      Canvas canvas, List<GeometryCommand> commands, DrawStyle drawStyle) {
-    final paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = drawStyle.lineWidth!.getWidth()
-      ..color = drawStyle.color;
-    final Path path = Path();
-    Offset offset = const Offset(0.0, 0.0);
-
-    for (var command in commands) {
-      switch (command.commandType) {
-        case GeometryCommandType.moveTo:
-          offset = _drawGeoMoveTo(path, command.commandParameters, offset);
-          break;
-        case GeometryCommandType.lineTo:
-          offset = _drawGeoLineTo(path, command.commandParameters, offset);
-          break;
-        case GeometryCommandType.closePath:
-          // nop
-          break;
-        default:
-      }
-    }
-
-    canvas.drawPath(path, paint);
-  }
-
-  void _drawPolygon(
-      Canvas canvas, List<GeometryCommand> commands, DrawStyle drawStyle) {
-    final paint = Paint()
-      ..style = PaintingStyle.fill
-      ..strokeWidth = 10.0
-      ..color = drawStyle.color;
-    final Path path = Path();
-    Offset offset = const Offset(0.0, 0.0);
-
-    for (var command in commands) {
-      switch (command.commandType) {
-        case GeometryCommandType.moveTo:
-          offset = _drawGeoMoveTo(path, command.commandParameters, offset);
-          break;
-        case GeometryCommandType.lineTo:
-          offset = _drawGeoLineTo(path, command.commandParameters, offset);
-          break;
-        case GeometryCommandType.closePath:
-          path.close();
-          break;
-        default:
-      }
-    }
-
-    canvas.drawPath(path, paint);
-  }
-
-  Offset _drawGeoMoveTo(Path path, List<Point<int>> cmdParams, Offset offset) {
-    Offset curOffset = offset;
-
-    for (var param in cmdParams) {
-      curOffset = curOffset.translate(param.x.toDouble(), param.y.toDouble());
-      path.moveTo(curOffset.dx, curOffset.dy);
-    }
-
-    return curOffset;
-  }
-
-  Offset _drawGeoLineTo(Path path, List<Point<int>> cmdParams, Offset offset) {
-    Offset curOffset = offset;
-
-    for (var param in cmdParams) {
-      curOffset = curOffset.translate(param.x.toDouble(), param.y.toDouble());
-      path.lineTo(curOffset.dx, curOffset.dy);
-    }
-
-    return curOffset;
   }
 
   @override
